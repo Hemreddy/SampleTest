10c29fa9cf0052cb410e61c9f5f51159cf16f3a1
Hemareddy Halli <hemareddyhalli@Hemareddys-MacBook-Pro.local>
2015-01-22 16:54:34 -0600

    Initial Commit

---
+++ SampleTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleTest
+//
+//  Created by Hemareddy Halli on 1/22/15.
+//  Copyright (c) 2015 Hemareddy Halli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleTest
+//
+//  Created by Hemareddy Halli on 1/22/15.
+//  Copyright (c) 2015 Hemareddy Halli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
