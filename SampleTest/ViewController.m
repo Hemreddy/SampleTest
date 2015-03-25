4d044b3518f0a7c76ebdc67e4b399cd1450f64c7
Hemreddy <hem.reddi@gmail.com>
2015-03-25 15:58:25 -0500

    Initial commit

--- SampleTest/ViewController.m
+++ SampleTest/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSMutableArray *theArray = [[ NSMutableArray alloc] initWithArray:nil];
+    NSLog(@"Barray :%@",theArray);
+    [theArray addObject:@"abc"];
+    NSLog(@"A array :%@",theArray);
+
 }
 
 - (void)didReceiveMemoryWarning {
